2da41f775d79955f9fac6b7e27c1c3718f29f9a6
RaissaKrupek <raissa.krupek@usp.br>
2024-08-15 21:01:49 -0300

    recent updates

--- R/mod_factorial.R
+++ R/mod_factorial.R
@@ -51,7 +51,18 @@ mod_factorial_ui <- function(id){
                                       selected = "none"), hr(),
                          p("If your data still not present the assumptions safter transformation, we suggest the usage of Generalized Linear Models (still not implemented in this app).")
                      ),
+                     hr(),
+                     box(width = 6,
+                         radioButtons(ns("assum7"), label = p("Choose the first factor to be evaluated:"),
+                                      choices = "Press 'Read the file' button to update",
+                                      selected = "Press 'Read the file' button to update"),
+                     ),
+                     box(width = 6,
+                         radioButtons(ns("assum8"), label = p("Choose the second factor to be evaluated:"),
+                                      choices = "Press 'Read the file' button to update",
+                                      selected = "Press 'Read the file' button to update"),
           
+                     ),                    
                      hr(),
                      box(width = 6,
                          radioButtons(ns("assum2"), label = p("Choose the traits to be evaluated"),
@@ -62,17 +73,15 @@ mod_factorial_ui <- function(id){
                          radioButtons(ns("assum3"), label = p("Choose the location to be evaluated:"),
                                       choices = "Press 'Read the file' button to update",
                                       selected = "Press 'Read the file' button to update")
-                         # ,
-                         # hr(),
-                         # actionButton(ns("assum5"), "Run analysis",icon("refresh")), br(),
+                        
                      ),
                      
-                     actionButton(ns("assum5"), "Run analysis", icon("refresh")), hr(),
-                     p("Expand the windows above to access the results")
+                     actionButton(ns("assum5"), "Run analysis", icon("refresh")), 
+                     hr(),
                      
-                     # box(width = 12,
-                     #     p("Expand the windows above to access the results")
-                     #     )
+                     box(width = 12,
+                     p("Expand the windows above to access the results")
+                         )
                  ),
                  
                  #Plots:
@@ -104,16 +113,28 @@ mod_factorial_ui <- function(id){
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Anova",
                      DT::dataTableOutput(ns("assum_anova_out"))
                  ),
+                 
+                 #Desdobramento
+                 box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Unfolding the interaction",
+                     DT::dataTableOutput(ns("assum_unfold_out"))
+                     
+                 ),
+                 
+                 #Shapiro-Wilk
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Shapiro-Wilk normality test",
                      p("H0: normal distribuition (p-value > 0.05)."),
                      p("H1: we can't consider that it is a normal distribuition (p-value < 0.05)."),
                      DT::dataTableOutput(ns("assum_sha_out"))
                  ),
+                 
+                 #Durbin-Watson
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Durbin-Watson Test for Autocorrelated Errors",
                      p("Computes residual autocorrelations and generalized Durbin-Watson statistics and their bootstrapped p-values."),
                      p("Used for normal distribuited longitudinal datasets. Usually, values of D-W between 1.5 and 2.5 indicate residual independence."),
                      DT::dataTableOutput(ns("assum_dur_out"))
                  ),
+                 
+                 #Breusch-Pagan 
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Breusch-Pagan Test",
                      p("Performs the Breusch-Pagan test against heteroskedasticity."),
                      p("Has as assumption normal distribuition."),
@@ -121,9 +142,13 @@ mod_factorial_ui <- function(id){
                      p("H1: we can't consider that there is homocedascity of variances (p-value < 0.05)."),
                      DT::dataTableOutput(ns("assum_bp_out"))
                  ),
+                 
+                 #Bonferroni-Holm
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Bonferroni-Holm tests for the adjusted p-values",
                      tableOutput(ns("assum_out_out"))
                  ),
+                 
+                 #Variance Inflation Factors
                  box(width = 12, solidHeader = TRUE, collapsible = TRUE, collapsed = T, status="info", title = "Variance Inflation Factors",
                      p("Checking multicollinearity: VIF value higher than 10 indicates multicollinearity."),
                      tableOutput(ns("assum_vif_out"))
@@ -200,6 +225,8 @@ mod_factorial_server <- function(input, output, session){
     dat
   })
   
+#As seguintes linhas filtrarao as opcoes de selecao 
+  
   observe({
     #I need to change that, it's the problem
     choices_trait_temp <- colnames(button_assum1())[-c(1:4)]
@@ -209,6 +236,7 @@ mod_factorial_server <- function(input, output, session){
     #Veremos no que dá
     choices_factor_temp <- colnames(button_assum1()[,3:4])
     
+    # just to check: print(choices_factor_temp)
     # to_remove <- c('local', 'gen')
     # choices_factor_temp[ , !(names(choices_factor_temp) %in% to_remove)]
     
@@ -234,6 +262,15 @@ mod_factorial_server <- function(input, output, session){
     choices_locations <- choices_locations_temp
     names(choices_locations) <- choices_locations_temp
     
+    updateRadioButtons(session, "assum7",
+                       label="Choose the first factor to be evaluated:",
+                       choices = choices_factor,
+                       selected = unlist(choices_factor)[1])
+    
+    updateRadioButtons(session, "assum8",
+                       label="Choose the second factor to be evaluated:",
+                       choices = choices_factor,
+                       selected = unlist(choices_factor)[1])
     
     updateRadioButtons(session, "assum2",
                        label="Choose the trait to be evaluated:",
@@ -250,21 +287,40 @@ mod_factorial_server <- function(input, output, session){
     withProgress(message = 'Building graphic', value = 0, {
       incProgress(0, detail = paste("Doing part", 1))
       dat <- button_assum1()
-      dat$gen <- as.factor(dat$gen)
+     
       dat$local <- as.factor(dat$local)
+      dat$factor1 <- as.factor(dat$gen)
+      dat$factor2 <- as.factor(dat$soil)
       
+#-------------------------------------------------------------------------------
+      #Treatment <- dat$gen:dat$soil
+      
+      #dados <- data.frame(dat,Treatment)
+      
+      ## Anova
+      #modelo <- lm(input$assum2 ~ Treatment, data = dados)
+      #anova(modelo)
       
       if(input$assum_design == "rbsp"){
-        if(!all(c("local", "block", "gen") %in% colnames(dat)) | ("rep" %in% colnames(dat)))
-          stop(safeError("Randomized block factorial arrangement design should have columns 'local', 'block', 'time', and 'gen'."))
+        if(!all(c("local", "block", "gen", "soil") %in% colnames(dat)) | ("rep" %in% colnames(dat)))
+          stop(safeError("Randomized block factorial arrangement design should have columns 'local', 'block', 'gen', and 'soil'."))
+        
         dat$block <- as.factor(dat$block)
+        
         #Mutio importante lembrar que é aqui onde os dados serão filtrados, disponibilizando apenas aqueles que serão analisados
-        dat <- dat %>% select(c("local", "block", input$assum2)) %>%
+        dat <- dat %>% select(c("local", "block",input$assum7, input$assum8, input$assum2)) %>%
           filter(local == input$assum3) %>% droplevels()
         #droplevels() - It's useful to remove unneeded factor levels
         
         dat$block <- as.random(dat$block)
         #o que o as.random() faz?
+        
+        #Montando os tratamentos 
+        #treat <- dat$gen:dat$soil
+        #dat <- data.frame(dat,treat)
+        #dados=data.frame(F1,F2,bloco,resp=NN)
+       
+
         
         if(input$assum4 == "none"){
           mod <- run_models_sp(df = dat, pheno = dat[,input$assum2] , whole_f = dat[,input$assum7] , split_f = dat[,input$assum8] , design = "RBSP", multi_env = F)
@@ -279,11 +335,13 @@ mod_factorial_server <- function(input, output, session){
         incProgress(0.5, detail = paste("Doing part", 2))
         
       } else {
-        if(!all(c("local", "block", "gen", "rep") %in% colnames(dat)))
-          stop(safeError("Completely randomized factorial arrangement design should have columns 'local', 'rep', 'time', and 'gen'."))
+        if(!all(c("local", "gen", "soil", "rep") %in% colnames(dat)))
+          stop(safeError("Completely randomized factorial arrangement design should have columns 'local', 'rep', 'gen', and 'maneg'."))
+        
         dat$rep <- as.factor(dat$rep)
+        
         #Just remember that it's an important step in the analysis's process
-        dat <- dat %>% select(c("local", "rep", input$assum2)) %>%
+        dat <- dat %>% select(c("local", "rep", input$assum7, input$assum8, input$assum2)) %>%
           filter(local == input$assum3) %>% droplevels()
         
         dat$rep <- as.random(dat$rep)
@@ -386,7 +444,7 @@ mod_factorial_server <- function(input, output, session){
     
     # Crie a tabela DataTable
     DT::datatable(data,
-                  rownames = c("block", paste(input$assum7), paste(input$assum8), paste("block:", input$assum7,  "\n(residual a)"), paste(input$assum7, input$assum8, sep = ":"), "residual"),
+                  rownames = c("block", paste(input$assum7), paste(input$assum8), paste(input$assum7, input$assum8, sep = ":"), "residual"),
                   extensions = 'Buttons',
                   options = list(
                     dom = 'Bfrtlp',
@@ -394,7 +452,35 @@ mod_factorial_server <- function(input, output, session){
                   ),
                   class = "display")
   })
+#------------------------------------------------------------------------------
+  output$assum_unfold_out <- DT::renderDataTable({
+    
+    data <- anova(button_assum2()[[1]])
   
+  # Especifique as colunas que deseja arredondar e o número de casas decimais
+  # columns_to_round <- c("Sum.Sq", "Mean.Sq", "F.value", "Pr..F.", "outra_coluna1", "outra_coluna2")
+  
+  # Arredonde as colunas selecionadas
+  decimal_places1 <- 2 
+  for (col in 2:4) {
+    data[[col]] <- round(as.numeric(data[[col]]), decimal_places1)
+  }
+  
+  decimal_places1 <- 5
+  for (col in 5) {
+    data[[col]] <- round(as.numeric(data[[col]]), decimal_places1)
+  }
+  
+  # Crie a tabela DataTable
+  DT::datatable(data,
+                rownames = c(paste(input$assum7), paste(input$assum8, ), paste(input$assum7, input$assum8, sep = ":"), "residual"),
+                extensions = 'Buttons',
+                options = list(
+                  dom = 'Bfrtlp',
+                  buttons = c('copy', 'csv', 'excel', 'pdf')
+                ),
+                class = "display")
+})
   
   output$assum_sha_out <- DT::renderDataTable(
     DT::datatable(button_assum2()[[2]],  
@@ -472,22 +558,24 @@ outlier <- function(resid, alpha=0.05){
   return(outliers_BH_df)
 }
 
-
+#-------------------------------------------------------------------------------
 ##' Utilities
 ##' 
 ##' @import MASS
-run_models_boxcox_sp <-function(pheno, whole_f, split_f, df, design, multi_env){
+run_models_boxcox_fac <-function(pheno, factor1, factor2, df, design, multi_env){
   if(design == "CRD"){
     if(multi_env){
       bc <- boxcox(pheno ~ df$gen + df$local/df$local + df$gen*df$local, plotit=F, lam=seq(-3, 3, 1/20))
       lambda <- bc$x[which.max(bc$y)]
       pheno_trans <- ((pheno^lambda-1)/lambda)
+      
       mod <- lm(pheno_trans ~ df$gen + df$local/df$local + df$gen*df$local)
       
     } else {
       bc <- boxcox(pheno ~ df$gen, plotit=F, lam=seq(-3, 3, 1/20))
       lambda <- bc$x[which.max(bc$y)]
       pheno_trans <- ((pheno^lambda-1)/lambda)
+      
       mod <- lm(pheno_trans ~ df$gen)
     }
   } else if(design == "DBC"){
@@ -495,6 +583,7 @@ run_models_boxcox_sp <-function(pheno, whole_f, split_f, df, design, multi_env){
       bc <- boxcox(pheno ~ df$gen + df$local/df$block + df$local + df$gen*df$local, plotit=F, lam=seq(-3, 3, 1/20))
       lambda <- bc$x[which.max(bc$y)]
       pheno_trans <- ((pheno^lambda-1)/lambda)
+      
       mod <- lm(pheno_trans ~ df$gen + df$local/df$block + df$local + df$gen*df$local)
       
     } else {
@@ -523,8 +612,8 @@ run_models_boxcox_sp <-function(pheno, whole_f, split_f, df, design, multi_env){
   } else if(design == "RBSP") {
     
     block <- as.random(df$block)
-    whole_f <- as.fixed(whole_f)
-    split_f <- as.fixed(split_f)
+    factor1 <- as.factor(factor1)
+    factor2 <- as.factor(factor2)
     
     # if(multi_env){
     #   bc <- boxcox(pheno ~ df$gen + df$local/df$rep/df$block +
@@ -535,18 +624,18 @@ run_models_boxcox_sp <-function(pheno, whole_f, split_f, df, design, multi_env){
     #   mod <- lm(pheno_trans ~ df$gen + df$local/df$rep/df$block +
     #               df$local/df$rep + df$local + df$gen*df$local)
     # } else {
-    bc <- boxcox(pheno ~ block + whole_f + whole_f/block + split_f + whole_f:split_f, plotit=F, lam=seq(-3, 3, 1/20))
+    bc <- boxcox(pheno ~ block + factor1 + factor2 + factor1:factor2, plotit=F, lam=seq(-3, 3, 1/20))
     lambda <- bc$x[which.max(bc$y)]
     pheno_trans <- ((pheno^lambda-1)/lambda)
     
-    mod <- lm(pheno_trans ~ block + whole_f + whole_f/block + split_f + whole_f:split_f)
+    mod <- lm(pheno_trans ~ block + factor1 + factor2 + factor1:factor2)
     #   }
   }
   else if(design == "CRSP") {
     
     rep <- as.random(df$rep)
-    whole_f <- as.fixed(whole_f)
-    split_f <- as.fixed(split_f)
+    factor1 <- as.factor(factor1)
+    factor2 <- as.factor(factor2)
     
     # if(multi_env){
     #   bc <- boxcox(pheno ~ df$gen + df$local/df$rep/df$block +
@@ -557,11 +646,11 @@ run_models_boxcox_sp <-function(pheno, whole_f, split_f, df, design, multi_env){
     #   mod <- lm(pheno_trans ~ df$gen + df$local/df$rep/df$block +
     #               df$local/df$rep + df$local + df$gen*df$local)
     # } else {
-    bc <- boxcox(pheno ~ whole_f + whole_f/rep + split_f + whole_f:split_f, plotit=F, lam=seq(-3, 3, 1/20))
+    bc <- boxcox(pheno ~ factor1 + factor2 + factor1:factor2, plotit=F, lam=seq(-3, 3, 1/20))
     lambda <- bc$x[which.max(bc$y)]
     pheno_trans <- ((pheno^lambda-1)/lambda)
     
-    mod <- lm(pheno_trans ~ whole_f + whole_f/rep + split_f + whole_f:split_f)
+    mod <- lm(pheno_trans ~ factor1 + factor2 + factor1:factor2)
     #   }
   }
   return(mod)
